4e7d2cd3cf6a118ad6aa4cff2acec9902ad5406d
ericwang994 <60726026+ericwang994@users.noreply.github.com>
2021-02-25 04:35:25 -0800

    part1

--- Part1-Jest/__tests__/main.test.js
+++ Part1-Jest/__tests__/main.test.js
@@ -1,19 +1,19 @@
 const formatVolumeIconPath  = require('../assets/scripts/main');
 
-describe('Main test group', () => {
-    test('Icon Level 3 Volume', () => {
+describe('formatVolumeIconPath test', () => {
+    test('Level 3', () => {
         expect(formatVolumeIconPath (67))
             .toMatch('./assets/media/icons/volume-level-3.svg');
     });
-    test('Icon Level 2 Volume', () => {
+    test('Level 2', () => {
         expect(formatVolumeIconPath (34))
             .toMatch('./assets/media/icons/volume-level-2.svg');
     });
-    test('Icon Level 1 Volume', () => {
+    test('Level 1', () => {
         expect(formatVolumeIconPath (1))
             .toMatch('./assets/media/icons/volume-level-1.svg');
     });
-    test('Icon Level 0 Volume', () => {
+    test('Level 0', () => {
         expect(formatVolumeIconPath (0))
             .toMatch('./assets/media/icons/volume-level-0.svg');
     });
